bdace7539026bc4f78ab1c27c47c488a3dadfd80
Neda <nedachangizi@gmail.com>
2022-11-15 14:54:29 -0800

    greet function

---
+++ scripts/script.js
@@ -0,0 +1,20 @@
+function greet(user, time = "day")
+{
+    console.log(`Good ${time} ${user}`);
+    // console.log('Hello ' + user);
+}
+greet("Neda");
+greet("Neda", "Night");
+// const greet = function (user)
+// {
+//     console.log(`Hello ${user}`);
+//     // console.log('Hello ' + user);
+// }
+
+// const greet =  (user) =>
+// {
+//     console.log(`Hello ${user}`);
+//     // console.log('Hello ' + user);
+// }
+
+

6acadfc27b7c98adb8415f56e33f886437135d4a
Neda <nedachangizi@gmail.com>
2022-11-15 15:56:23 -0800

    shopping list

--- scripts/script.js
+++ scripts/script.js
@@ -17,20 +17,34 @@
 //     // console.log('Hello ' + user);
 // }
 
-let r = prompt("Enter a radius.");
-console.log(r)
+// let r = prompt("Enter a radius.");
+// console.log(r)
 
-function circleArea (radius)
+// function circleArea (radius)
+// {
+//     const radiusP = document.querySelector("#radius");
+//     const resultP = document.querySelector("#result");
+//     radiusP.textContent =radius;
+//     let calculatedArea = Math.PI * radius * radius;
+//     resultP.textContent = calculatedArea
+//     return calculatedArea;
+// }
+
+// let area = circleArea(r).toFixed(2);
+
+// alert(`The area of a circle with radius ${r} is ${area}`)
+const unorderedList = document.querySelector(".shopping");
+ 
+
+function populateList(list)
 {
-    const radiusP = document.querySelector("#radius");
-    const resultP = document.querySelector("#result");
-    radiusP.textContent =radius;
-    let calculatedArea = Math.PI * radius * radius;
-    resultP.textContent = calculatedArea
-    return calculatedArea;
+    for (let i = 0 ; i < list.length; i++)
+    {
+       let listItem =  document.createElement("li");
+       listItem.textContent = list[i]
+       unorderedList.appendChild(listItem);
+    }
 }
 
-let area = circleArea(r).toFixed(2);
-
-alert(`The area of a circle with radius ${r} is ${area}`)
-
+const shoppingList = ["cheese", "bread", "green pepper"];
+populateList(shoppingList);

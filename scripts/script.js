2a8fabdc70a2199c4e672cfe23da017bd3bb32e6
Neda <nedachangizi@gmail.com>
2022-11-15 15:16:41 -0800

    circle area function

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,10 @@
-function greet(user, time = "day")
-{
-    console.log(`Good ${time} ${user}`);
-    // console.log('Hello ' + user);
-}
-greet("Neda");
-greet("Neda", "Night");
+// function greet(user, time = "day")
+// {
+//     console.log(`Good ${time} ${user}`);
+//     // console.log('Hello ' + user);
+// }
+// greet("Neda");
+// greet("Neda", "Night");
 // const greet = function (user)
 // {
 //     console.log(`Hello ${user}`);
@@ -17,4 +17,15 @@ greet("Neda", "Night");
 //     // console.log('Hello ' + user);
 // }
 
+let r = prompt("Enter a radius.");
+console.log(r)
+
+function circleArea (radius)
+{
+    return Math.PI * radius * radius;
+}
+
+let area = circleArea(r).toFixed(2);
+console.log(radius)
+alert(`The area of a circle with radius ${r} is ${area}`)
 
